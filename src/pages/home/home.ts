5e3d9f4df2ff31ec95dd8f6db594c79207333670
Anderson Araujo da Silva <f585414@corp.caixa.gov.br>
2020-05-08 02:22:47 -0300

    Arquivos da idéia inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  protected listaItens: any;
+  constructor(public navCtrl: NavController, 
+    public storage: Storage,
+    public alertCrtl: AlertController) {
+  }
+  adicionar() {
+    let prompt = this.alertCrtl.create({
+      title: 'Adicionar Item a Lista',
+      message: 'Entre com o nome do item que deseja adicionar à lista:',
+      inputs: [
+        {
+          name: 'item',
+          placeholder: 'Nome do item...'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: (data) => {
+            // Botao cancelar clicado, nao fazer nada
+          }
+        },
+        {
+          text: 'Adicionar',
+          handler: (data) => {
+            if (!Array.isArray(this.listaItens) || !this.listaItens.length) {
+              this.listaItens = new Array();
+            }
+            this.listaItens.push(
+              {
+                nome: data.item,
+                marcado: false
+              }
+            );
+            this.storage.set('listaItens', this.listaItens);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
+  ionViewDidLoad(){
+    this.storage.get('listaItens').then(
+      (val) => {
+        this.listaItens = val;
+      }
+    )
+  }
 }
